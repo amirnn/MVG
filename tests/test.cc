810ebdc9386022abc2a982ec88201c3a2451217a
Amir Nourinia <amir.nourinia@gmail.com>
2022-07-28 09:04:16 +0200

    build success

--- tests/test.cc
+++ tests/test.cc
@@ -1,7 +1,14 @@
 // Copyright 2021 Amir Nourinia
 // #include <gtest/gtest.h>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
-TEST(Algorithms, first_test){
-    EXPECT_EQ(1 , 1);
+// TEST(Algorithms, first_test){
+//     EXPECT_EQ(1 , 1);
+// }
+
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("first test"){
+    CHECK(factorial(1) == 1);
 }
